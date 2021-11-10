ebcbe5da00c7c0fe35f08775b18b461ea6658db0
Aubry Franquinet <aubry.franquinet@student.vinci.be>
2021-11-10 14:41:14 +0100

    added previous and next function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-//on click function getPokemon on button
+//SEARCH POKEMON IN SEARCH INPUT
 document.getElementById("getPokemon").addEventListener("click", getPokemon);
 
 function getPokemon() {
@@ -8,33 +8,194 @@ function getPokemon() {
     fetch(`https://pokeapi.co/api/v2/pokemon/` + pokemon)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            
 
             let div = document.getElementById("pokemonInfo");
             let img = document.getElementById('pokemonImg');
 
-            div.innerHTML =
-                '<h4> ID : </h4>' + data.id + '<br>' +
-                '<h4> Name : </h4>' + data.name + '<br>' +
-                '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>' + '- ' + data.moves[1].move.name + '<br>' + '- ' + data.moves[2].move.name + '<br>';
 
-            img.innerHTML = '<img src="' + data.sprites.front_default + '">';
 
-            //if data.move[1] is undefined, then it will not show the second move
-            if (data.moves[1] === undefined) {
-                div.innerHTML += '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>';
+            //display only first move of pokemon if he has only one move
+            if (data.moves[0].move.name == "transform") {
+                div.innerHTML =
+                    '<h4 id = "ID"> ID : </h4>' + '<p id="dataID">' + data.id + '</p>' + '<br>' +
+                    '<h4> Name : </h4>' + data.name + '<br>' +
+                    '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>';
+
+                img.innerHTML = '<img src="' + data.sprites.front_default + '">';
+            }
+            else {
+                div.innerHTML =
+                    '<h4 id = "ID"> ID : </h4>' + '<p id="dataID">' + data.id + '</p>' + '<br>' +
+                    '<h4> Name : </h4>' + data.name + '<br>' +
+                    '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>' + '- ' + data.moves[1].move.name + '<br>' + '- ' + data.moves[2].move.name + '<br>';
+
+                img.innerHTML = '<img src="' + data.sprites.front_default + '">';
             }
         }
 
         )
-
+    //GET EVOLUTION 1 OF POKEMON IF IT EXIST
     fetch(`https://pokeapi.co/api/v2/pokemon-species/` + pokemon)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+           
 
             let previousEvolution = document.getElementById("pokemonInfo");
             previousEvolution.innerHTML +=
                 '<h4> Previous evolution : </h4>' + data.evolves_from_species.name;
         })
+}
+
+
+//GET NEXT POKEMON WHEN CLICK ON NEXT BUTTON 
+document.getElementById("next").addEventListener("click", nextPokemon );
+function nextPokemon() {
+    let nxtPokemon = document.getElementById("dataID").innerText;
+    
+    
+    
+    nxtPokemon = parseInt(nxtPokemon);
+    nxtPokemon++;
+    
+
+    
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/` + nxtPokemon)
+        .then(response => response.json())
+        .then(data => {
+            
+
+            let div = document.getElementById("pokemonInfo");
+            let img = document.getElementById('pokemonImg');
+
+            //display only first move of pokemon if he has only one move
+            if (data.moves[0].move.name == "transform") {
+                div.innerHTML =
+                    '<h4 id = "ID"> ID : </h4>' + '<p id="dataID">' + data.id + '</p>' + '<br>' +
+                    '<h4> Name : </h4>' + data.name + '<br>' +
+                    '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>';
+
+                img.innerHTML = '<img src="' + data.sprites.front_default + '">';
+            }
+            else {
+                div.innerHTML =
+                    '<h4 id = "ID"> ID : </h4>' + '<p id="dataID">' + data.id + '</p>' + '<br>' +
+                    '<h4> Name : </h4>' + data.name + '<br>' +
+                    '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>' + '- ' + data.moves[1].move.name + '<br>' + '- ' + data.moves[2].move.name + '<br>';
+
+                img.innerHTML = '<img src="' + data.sprites.front_default + '">';
+            }
+        }
+
+        )
+
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/` + nxtPokemon)
+        .then(response => response.json())
+    fetch(`https://pokeapi.co/api/v2/pokemon/` + nxtPokemon)
+        .then(response => response.json())
+        .then(data => {
+            
+
+            let div = document.getElementById("pokemonInfo");
+            let img = document.getElementById('pokemonImg');
+
+
+
+            //display only first move of pokemon if he has only one move
+            if (data.moves[0].move.name == "transform") {
+                div.innerHTML =
+                    '<h4 id = "ID"> ID : </h4>' + '<p id="dataID">' + data.id + '</p>' + '<br>' +
+                    '<h4> Name : </h4>' + data.name + '<br>' +
+                    '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>';
+
+                img.innerHTML = '<img src="' + data.sprites.front_default + '">';
+            }
+            else {
+                div.innerHTML =
+                    '<h4 id = "ID"> ID : </h4>' + '<p id="dataID">' + data.id + '</p>' + '<br>' +
+                    '<h4> Name : </h4>' + data.name + '<br>' +
+                    '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>' + '- ' + data.moves[1].move.name + '<br>' + '- ' + data.moves[2].move.name + '<br>';
+
+                img.innerHTML = '<img src="' + data.sprites.front_default + '">';
+            }
+        }
+
+        )
+}
+
+//GET NEXT POKEMON WHEN CLICK ON NEXT BUTTON 
+document.getElementById("past").addEventListener("click", previousPokemon);
+function previousPokemon() {
+    let pastPokemon = document.getElementById("dataID").innerText;
+
+    
+
+    pastPokemon = parseInt(pastPokemon);
+    pastPokemon--;
+    
+
+
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/` + pastPokemon)
+        .then(response => response.json())
+        .then(data => {
+
+
+            let div = document.getElementById("pokemonInfo");
+            let img = document.getElementById('pokemonImg');
+
+            //display only first move of pokemon if he has only one move
+            if (data.moves[0].move.name == "transform") {
+                div.innerHTML =
+                    '<h4 id = "ID"> ID : </h4>' + '<p id="dataID">' + data.id + '</p>' + '<br>' +
+                    '<h4> Name : </h4>' + data.name + '<br>' +
+                    '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>';
+
+                img.innerHTML = '<img src="' + data.sprites.front_default + '">';
+            }
+            else {
+                div.innerHTML =
+                    '<h4 id = "ID"> ID : </h4>' + '<p id="dataID">' + data.id + '</p>' + '<br>' +
+                    '<h4> Name : </h4>' + data.name + '<br>' +
+                    '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>' + '- ' + data.moves[1].move.name + '<br>' + '- ' + data.moves[2].move.name + '<br>';
+
+                img.innerHTML = '<img src="' + data.sprites.front_default + '">';
+            }
+        }
+
+        )
+
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/` + pastPokemon)
+        .then(response => response.json())
+    fetch(`https://pokeapi.co/api/v2/pokemon/` + pastPokemon)
+        .then(response => response.json())
+        .then(data => {
+            
+
+            let div = document.getElementById("pokemonInfo");
+            let img = document.getElementById('pokemonImg');
+
+
+
+            //display only first move of pokemon if he has only one move
+            if (data.moves[0].move.name == "transform") {
+                div.innerHTML =
+                    '<h4 id = "ID"> ID : </h4>' + '<p id="dataID">' + data.id + '</p>' + '<br>' +
+                    '<h4> Name : </h4>' + data.name + '<br>' +
+                    '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>';
+
+                img.innerHTML = '<img src="' + data.sprites.front_default + '">';
+            }
+            else {
+                div.innerHTML =
+                    '<h4 id = "ID"> ID : </h4>' + '<p id="dataID">' + data.id + '</p>' + '<br>' +
+                    '<h4> Name : </h4>' + data.name + '<br>' +
+                    '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>' + '- ' + data.moves[1].move.name + '<br>' + '- ' + data.moves[2].move.name + '<br>';
+
+                img.innerHTML = '<img src="' + data.sprites.front_default + '">';
+            }
+        }
+
+        )
 }
