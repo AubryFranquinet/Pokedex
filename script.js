0d5239e110a5fc9d92352baa686e0017f6471afd
Aubry Franquinet <aubry.franquinet@student.vinci.be>
2021-11-10 11:41:01 +0100

    minor changes

--- script.js
+++ script.js
@@ -13,14 +13,20 @@ function getPokemon() {
             let div = document.getElementById("pokemonInfo");
             let img = document.getElementById('pokemonImg');
 
-            div.innerHTML = 
-            ' ID : ' + data.id + '<br>' + 
-            ' Name : ' + data.name + '<br>';
+            div.innerHTML =
+                '<h4> ID : </h4>' + data.id + '<br>' +
+                '<h4> Name : </h4>' + data.name + '<br>' +
+                '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>' + '- ' + data.moves[1].move.name + '<br>' + '- ' + data.moves[2].move.name + '<br>';
 
             img.innerHTML = '<img src="' + data.sprites.front_default + '">';
-            
 
-        })
+            //if data.move[1] is undefined, then it will not show the second move
+            if (data.moves[1] === undefined) {
+                div.innerHTML += '<h4> Moves : </h4>' + '- ' + data.moves[0].move.name + '<br>';
+            }
+        }
+
+        )
 
     fetch(`https://pokeapi.co/api/v2/pokemon-species/` + pokemon)
         .then(response => response.json())
@@ -28,7 +34,7 @@ function getPokemon() {
             console.log(data);
 
             let previousEvolution = document.getElementById("pokemonInfo");
-            previousEvolution.innerHTML += 
-            ' Previous evolution : ' + data.evolves_from_species.name;
+            previousEvolution.innerHTML +=
+                '<h4> Previous evolution : </h4>' + data.evolves_from_species.name;
         })
 }
